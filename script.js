888165fde084b15208146f863849a777700e7be6
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2023-10-28 11:22:02 +0100

    fix the mobile navigation bar issues

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ let dropdownIcon = document.getElementById("dropdownMenuToggleIcon");
 let backdrop = document.getElementById("backdrop");
 let defaultMenu = document.getElementById("rightNavSect");
 let smNavBar = document.getElementById("smallScreenNavBar");
+let smNavCloseBtn = document.getElementById("closeSmNav")
 
 dropdownIcon.addEventListener("click", () => {
   //   if (defaultMenu.classList.contains("hidden"))
-  defaultMenu.classList.add("hidden");
   backdrop.classList.remove("hidden");
   smNavBar.classList.remove("hidden")
+  defaultMenu.classList.add("hidden");
 });
+
+
+smNavCloseBtn.addEventListener("click", ()=>{
+    backdrop.classList.add("hidden");
+    smNavBar.classList.add("hidden")
+})
